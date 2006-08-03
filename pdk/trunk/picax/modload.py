628e6619dba51c588211729eafd4caf4e6cf5d01
branden <branden@progeny.com>
2006-08-03 15:44:37 -0400

    [svn r5520] Update picax licensing boilerplates to PDK versions.  This also updates the FSF's address to its current location.

--- pdk/trunk/picax/modload.py
+++ pdk/trunk/picax/modload.py
@@ -2,21 +2,23 @@
 #
 # Code for loading modules.
 #
-# Copyright 2003, 2004, 2005 Progeny Linux Systems.
+#   Copyright 2003, 2004, 2005 Progeny Linux Systems, Inc.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# at your option) any later version.
+#   This file is part of PDK.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#   PDK is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#   PDK is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PDK; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 """This module does the heavy lifting of importing external modules for
 the installer and media subsystems."""
