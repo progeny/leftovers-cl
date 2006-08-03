9fc1047506a71b8658b90e19642c2b7f2efae873
branden <branden@progeny.com>
2006-08-03 15:41:41 -0400

    [svn r5518] Add licensing information.

--- pdk/trunk/hashfile.py
+++ pdk/trunk/hashfile.py
@@ -4,8 +4,24 @@
 #
 # Provide a file wrapper object for calculating hashes.
 #
-# Copyright 2003 Progeny Linux Systems.
+#   Copyright 2003 Progeny Linux Systems, Inc.
 #
+#   This file is part of PDK.
+#
+#   PDK is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   PDK is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PDK; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
 # This module provides a way to auto-update one or more Python hash
 # calculators as I/O is performed on a file object.  Hash objects that
 # conform to the standard interface for hash objects as implemented by
