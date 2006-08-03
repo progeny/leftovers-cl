7d3a15496a021bc78dc89c7da8e22789d0aa919d
branden <branden@progeny.com>
2006-08-03 15:43:19 -0400

    [svn r5519] Add copyright and licensing information to files lacking them and which have substantial amounts of code.

--- pdk/trunk/picax/installer.py
+++ pdk/trunk/picax/installer.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 
 # $Progeny$
+#
+#   Copyright 2003, 2004, 2005 Progeny Linux Systems, Inc.
+#
+#   This file is part of PDK.
+#
+#   PDK is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   PDK is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PDK; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 """This module provides a public interface to an installer add-on module
 requested in the configuration, as well as providing the mechanism for
--- pdk/trunk/picax/media.py
+++ pdk/trunk/picax/media.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python
 
 # $Progeny$
+#
+#   Copyright 2004, 2005 Progeny Linux Systems, Inc.
+#
+#   This file is part of PDK.
+#
+#   PDK is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   PDK is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+#   License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with PDK; if not, write to the Free Software Foundation,
+#   Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 """This module provides a public interface to the add-on media modules,
 as well as providing a mechanism for loading such modules."""
